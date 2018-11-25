5ed5f583dcba527d1fc7b9369e2426248331d1c9
abbie <2807076495@qq.com>
2018-11-25 23:49:53 +0800

    dev

---
+++ src/wing_client/main.go
@@ -0,0 +1,74 @@
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	pb "wing_server/modules/sandbox/proto"
+	"google.golang.org/grpc/credentials/oauth"
+	"golang.org/x/oauth2"
+	"google.golang.org/grpc/credentials"
+	"crypto/tls"
+)
+
+const (
+	PORT = ":15747"
+	TOKEN = "V9max5VOMkt3q="
+)
+
+// fetchToken simulates a token lookup and omits the details of proper token
+// acquisition. For examples of how to acquire an OAuth2 token, see:
+// https://godoc.org/golang.org/x/oauth2
+func fetchToken() *oauth2.Token {
+	return &oauth2.Token{
+		AccessToken: TOKEN,
+	}
+}
+
+func main() {
+	perRPC := oauth.NewOauthAccess(fetchToken())
+	opts := []grpc.DialOption{
+		grpc.WithPerRPCCredentials(perRPC),
+		grpc.WithTransportCredentials(
+			credentials.NewTLS(&tls.Config{InsecureSkipVerify: true}),
+		),
+	}
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(PORT, opts...)
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewSandboxClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.Compile(ctx, &pb.Input{Language: "php", Data: "<?php phpinfo();", Watchdog:120})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	if r.Data == "" {
+		log.Println("empty")
+	}
+	log.Printf("Greeting: %s", r)
+}
---
+++ src/wing_server/modules/sandbox/index.go
@@ -0,0 +1,94 @@
+package sandbox
+
+import (
+	"context"
+	pb "wing_server/modules/sandbox/proto"
+	"google.golang.org/grpc"
+	"os"
+	"log"
+	"errors"
+	"fmt"
+	"os/exec"
+	"wing_server/modules/sandbox/config"
+	"crypto/md5"
+	"encoding/hex"
+	"io/ioutil"
+	"strings"
+	"strconv"
+)
+
+type server struct{}
+
+func (s *server) Compile (ctx context.Context, in *pb.Input) (*pb.Output, error) {
+	script := config.Sandbox[in.Language]
+	if script == nil {
+		return nil, errors.New("尚不支持的编译语言")
+	}
+	md5Ctx := md5.New()
+	md5Ctx.Write([]byte(in.Language + in.Data))
+	cipher := hex.EncodeToString(md5Ctx.Sum(nil))
+	filename := fmt.Sprintf("/tmp/%v", cipher)
+	_, err := os.Stat(filename)
+	if err != nil {
+		err = ioutil.WriteFile(filename, []byte(in.Data), 0644)
+		if err != nil {
+			return nil, errors.New("创建文件失败")
+		}
+	}
+	command := fmt.Sprintf("/usr/local/bin/wing.sh %d %v %v", in.Watchdog, filename, script.Filename)
+	switch in.Language {
+	case "mysql":
+		command = fmt.Sprintf("%v -u mysql", command)
+	case "nodejs":
+		command = fmt.Sprintf("%v -e NODE_PATH=/usr/local/lib/node_modules", command)
+	}
+
+	command = fmt.Sprintf("%v %v%v \"/usr/local/bin/script.sh %v %v %v %v\"",
+		command, config.DockerAddress, script.DockerName, script.CompilerName,
+		script.Filename, script.OutputCmd, script.Arguments)
+	log.Println(command)
+
+	cmd := exec.Command("/bin/bash", "-c", command)
+	out, err := cmd.Output()
+	if err != nil {
+		log.Println(err)
+		return nil, errors.New("执行脚本失败")
+	}
+	response := string(out)
+	if response == "" || !strings.Contains(response, config.EOF) {
+		return nil, errors.New("执行超时")
+	}
+	pos := strings.LastIndex(response, config.EOF)
+	data := response[0:pos]
+	runtime, err := strconv.ParseFloat(response[pos + len(config.EOF):], 64)
+	if err != nil {
+		runtime = 0.00
+	}
+	return &pb.Output{
+		Language: script.Language,
+		Runtime: runtime,
+		Data: data,
+	}, nil;
+}
+
+func Register (grpc *grpc.Server) {
+	env := os.Getenv("ENV")
+	if env == "production" {
+		for _, script := range config.Sandbox {
+			inspect := fmt.Sprintf("docker inspect %v%v >/dev/null", config.DockerAddress, script.DockerName)
+			cmd := exec.Command(inspect)
+			out, err := cmd.Output()
+			log.Println(err, string(out))
+			if err != nil || len(out) > 0 {
+				pull := fmt.Sprintf("docker pull %v%v >/dev/null", config.DockerAddress, script.DockerName)
+				cmd = exec.Command(pull)
+				out, err := cmd.Output()
+				log.Println(err, string(out))
+				if err != nil || len(out) > 0 {
+					log.Fatal(err, string(out))
+				}
+			}
+		}
+	}
+	pb.RegisterSandboxServer(grpc, &server{});
+}
