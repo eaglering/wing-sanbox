f44019a5318471a2047b18e5500bc6827c5c2c29
abbie <2807076495@qq.com>
2018-12-01 16:32:22 +0800

    dev

--- bin/wing.sh
+++ bin/wing.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
-set -e
 
 WATCHDOG=$1
 shift
-
 FILENAME=$1
 shift
-
 DESTNAME=$1
 shift
 
 cont=$(docker create --cpus=1 -m 512M --rm "$@" 2>/dev/null)
 if [ -z "$cont" ]; then
     echo "获取编译器失败"
-    exit 0
-fi
-docker cp $FILENAME $cont:/data/$DESTNAME >/dev/null 2>&1
-docker start -a $cont &
-code=$(timeout -t "${WATCHDOG}" docker wait "$cont" || true)
+else
+    docker cp $FILENAME $cont:/data/$DESTNAME >/dev/null 2>&1
+    docker start -a $cont &
+    code=$(timeout -t "${WATCHDOG}" docker wait "$cont" || true)
 
-if [ -z "$code" ]; then
-    docker kill $cont >/dev/null 2>&1
+    if [ -z "$code" ]; then
+        docker kill $cont >/dev/null 2>&1
+    fi
 fi
